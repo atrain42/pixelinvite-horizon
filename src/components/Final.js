e99a0cc5a692abd71ff838490fde50d2f0379ffa
atrain42 <maxheimer4211@gmail.com>
2022-09-12 20:13:18 -0400

    updates

--- src/components/Final.js
+++ src/components/Final.js
@@ -1,13 +1,42 @@
+import { useRef } from "react";
+import { useInView } from "framer-motion";
 import "./Final.css";
 
 import final from "../img/final.jpg";
 
 const Final = () => {
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: true });
+
   return (
-    <section className="final">
-      <img src={final} alt="happy couple" />
-      <h1>Love you guys!</h1>
-      <p>We hope to see you all at the shower</p>
+    <section className="final" ref={ref}>
+      <img
+        src={final}
+        alt="happy couple"
+        style={{
+          transform: isInView ? "none" : "translateY(25px)",
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.75s ease-in 0.25s",
+        }}
+      />
+      <h1
+        style={{
+          transform: isInView ? "none" : "translateY(25px)",
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.5s ease-in 0.75s",
+        }}
+      >
+        Love you guys!
+      </h1>
+      <p
+        style={{
+          transform: isInView ? "none" : "translateY(25px)",
+          opacity: isInView ? 1 : 0,
+          transition: "all 0.5s ease-in 1s",
+        }}
+      >
+        We hope to see you all at the shower
+      </p>
     </section>
   );
 };
