5ee82d2c7906fe7d9651545a5a3d7c65416c7cbd
atrain42 <maxheimer4211@gmail.com>
2023-12-19 11:34:29 -0500

    updates

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,28 @@
 import "./Contact.css";
 
 const Contact = () => {
+  const onSubmit = () => {
+    fetch('https://formsubmit.co/ajax/pixelprism42@gmail.com', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({
+        name: 'FormSubmit',
+        message: "I'm from Devro LABS",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.log(error))
+  }
   return (
     <section className="contact" id="rsvp">
       <h1>RSVP</h1>
-      <form>
+      <form action="https://formsubmit.co/pixelprism42@gmail.com" method="POST">
+        <input type="hidden" name="_subject" value="Someone just RSVP'd!"></input>
+        <input type="hidden" name="_autoresponse" value="your custom message" />
         <input
           type="text"
           name="First&nbsp;Name"
@@ -23,7 +41,6 @@ const Contact = () => {
             type="radio"
             name="Are&nbsp;you&nbsp;attending"
             value="Yes"
-            required
             className="radio-btn"
           />
           <label htmlFor="I'm coming" c>
@@ -35,13 +52,12 @@ const Contact = () => {
             type="radio"
             name="Are&nbsp;you&nbsp;attending *"
             value="No"
-            required
             className="radio-btn"
           />
           <label htmlFor="I cant't make it">Decline with regret</label>
         </span>
         <textarea name="" cols="20" rows="15" placeholder="Message"></textarea>
-        <button>submit</button>
+        <button onClick={onSubmit}>submit</button>
       </form>
     </section>
   );
